3f2b5a23bc799b70463deaf862c07fdbc78b2f61
10Dozen <steel.mjr@gmail.com>
2015-04-07 00:20:33 +0300

    Update config.cpp

--- x_dzn_acex_rhsus/config.cpp
+++ x_dzn_acex_rhsus/config.cpp
@@ -20,27 +20,68 @@ class CfgPatches
 };
 
 
-#define	SOUND_M4
-#define	SOUND_M4_SD
-#define	SOUND_M4_BURST
-#define	SOUND_M4_BURST_SD
-#define	SOUND_M4_FA
-#define	SOUND_M4_FA_SD
-
-#define	SOUND_M16
-#define	SOUND_M16_SD
-#define	SOUND_M16_BURST
-#define	SOUND_M16_BURST_SD
 
 
-#define	SOUND_M249
-#define	SOUND_M249_SD
+// M4 *****************************************
+#define SOUND_M4				begin1[] = {"\SOUND_PATH\acex_sound\m4_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\m4_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\m4_fire2.wss",1.77828,0.97,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_M4_SD				begin1[] = {"\SOUND_PATH\acex_sound\m4sd_fire.wss",1,1,80};begin2[] = {"\SOUND_PATH\acex_sound\m4sd_fire.wss",1,0.96,80};begin3[] = {"\SOUND_PATH\acex_sound\m4sd_fire.wss",1,1.02,80};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_M4_BURST			SOUND_M4
+#define SOUND_M4_BURST_SD		SOUND_M4_SD
+#define SOUND_M4_FA				begin1[] = {"\SOUND_PATH\acex_sound\m4_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\m4_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\m4_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.2,"begin2",0.2,"begin3",0.6};
+#define SOUND_M4_FA_SD			begin1[] = {"\SOUND_PATH\acex_sound\m4sd_fire.wss",1,1,80};begin2[] = {"\SOUND_PATH\acex_sound\m4sd_fire.wss",1,0.96,80};begin3[] = {"\SOUND_PATH\acex_sound\m4sd_fire.wss",1,1.03,80};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
 
-#define	SOUND_M240
-#define	SOUND_M240_SD
 
-#define SOUND_GL
-#define SOUNG_GL_RELOAD
+// M16 ****************************************
+#define SOUND_M16				begin1[] = {"\SOUND_PATH\acex_sound\m16_fire1.wss",1.77828,1,1100};soundBegin[] = {"begin1",1};
+#define SOUND_M16_SD			SOUND_M4_SD
+#define SOUND_M16_BURST			begin1[] = {"\SOUND_PATH\acex_sound\m16_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",1};
+#define SOUND_M16_BURST_SD		SOUND_M4_BURST_SD
+#define SOUND_M16_FA			begin1[] = {"\SOUND_PATH\acex_sound\m16_fire3.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\m16_fire3.wss",1.77828,0.97,1100};begin3[] = {"\SOUND_PATH\acex_sound\m16_fire3.wss",1.77828,1.03,1100};soundBegin[] = {"begin1",0.32,"begin2",0.35,"begin3",0.32};
+#define SOUND_M16_FA_SD			SOUND_M4_FA_SD
+
+
+// Mk12 and other AR-15 based rifles **********
+#define SOUND_MK12				begin1[] = {"\SOUND_PATH\acex_sound\mk12_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\mk12_fire2.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.5,"begin2",0.5};
+#define SOUND_MK12_SD			SOUND_M4_SD
+
+#define SOUND_M110				begin1[] = {"\SOUND_PATH\acex_sound\mk17_fire1.wss",1.77828,1,1700};begin2[] = {"\SOUND_PATH\acex_sound\mk17_fire2.wss",1.77828,1,1700};begin3[] = {"\SOUND_PATH\acex_sound\mk17_fire2.wss",1.77828,0.98,1700};soundBegin[] = {"begin1",0.33,"begin2",0.34,"begin3",0.33};
+#define SOUND_M110_SD			begin1[] = {"\SOUND_PATH\acex_sound\mk17sd_fire.wss",1,1,110};soundbegin[] = {"begin1",1};
+
+#define SOUND_HB				SOUND_M4
+#define SOUND_HB_SD				SOUND_M4_SD
+#define SOUND_HB_FA				SOUND_M4_FA
+#define SOUND_HB_FA_SD			SOUND_M4_FA_SD
+
+#define SOUND_SBR9				SOUND_M4
+#define SOUND_SBR9_SD			SOUND_M4_SD
+#define SOUND_SBR9_FA			SOUND_M4_FA
+#define SOUND_SBR9_FA_SD		SOUND_M4_FA_SD
+
+// HK416 **************************************
+#define SOUND_416				SOUND_M4
+#define SOUND_416_SD			SOUND_M4_SD
+#define SOUND_416_FA			SOUND_M4_FA
+#define SOUND_416_FA_SD			SOUND_M4_FA_SD
+
+
+// M249 ***************************************
+#define	SOUND_M249				begin1[] = {"\SOUND_PATH\acex_sound\m249p_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\m249p_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\m249p_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define	SOUND_M249_SD			SOUND_M249
+
+#define	SOUND_M249SAW			begin1[] = {"\SOUND_PATH\acex_sound\m249_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\m249_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\m249_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define	SOUND_M249SAW_SD		SOUND_M249SAW
+
+// M240 ***************************************
+#define	SOUND_M240				begin1[] = {"\SOUND_PATH\acex_sound\m240_fire1.wss",1.77828,1,1700};begin2[] = {"\SOUND_PATH\acex_sound\m240_fire2.wss",1.77828,1,1700};begin3[] = {"\SOUND_PATH\acex_sound\m240_fire3.wss",1.77828,1,1700};soundBegin[] = {"begin1",0.33,"begin2",0.34,"begin3",0.33};
+#define	SOUND_M240_SD			SOUND_M240
+
+
+// M203/AG36 **********************************
+#define	SOUND_GL				sound[]={"\SOUND_PATH\acex_sound\m203_fire.wss",1,1,100};
+#define	SOUND_GL_RELOAD			reloadMagazineSound[] = {"\SOUND_PATH\acex_sound\m203_reload.wss",0.0316228,1,20};
+
+
+
 
 
 class Mode_SemiAuto;
