6237f83bd063d45b83b7ec35ae2906f01a56ebdb
10Dozen <steel.mjr@gmail.com>
2015-04-06 19:30:30 +0300

    Update defines.cpp

--- CommonConfig/defines.cpp
+++ CommonConfig/defines.cpp
@@ -4,27 +4,56 @@
 // AK 74 **************************************
 #define SOUND_AK74				begin1[]={"\SOUND_PATH\acex_sound\ak74_fire1",1.77828,1,1100};begin2[]={"\SOUND_PATH\acex_sound\ak74_fire2",1.77828,1,1100};begin3[]={"\SOUND_PATH\acex_sound\ak74_fire3",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
 #define SOUND_AK74_SD			begin1[]={"\SOUND_PATH\acex_sound\aks74un_fire",1,1,80};begin2[]={"\SOUND_PATH\acex_sound\aks74un_fire",1,0.97,80};begin3[]={"\SOUND_PATH\acex_sound\aks74un_fire",1,1.016,80};soundBegin[]={"begin1",0.33,"begin2",0.33,"begin3",0.33};	
+#define SOUND_AK74_FA
+#define SOUND_AK74_FA_SD
 
 #define SOUND_AEK				SOUND_AK74
 #define SOUND_AEK_SD			SOUND_AK74_SD
+#define SOUND_AEK_FA			SOUND_AK74
+#define SOUND_AEK_FA_SD			SOUND_AK74_SD
+#define SOUND_AEK_BURST			SOUND_AK74
+#define SOUND_AEK_BURST_SD		SOUND_AK74_SD
 
+#define SOUND_AK12
+#define SOUND_AK12_SD
+#define SOUND_AK12_FA
+#define SOUND_AK12_FA_SD
 #define SOUND_AK12_BURST		SOUND_AK74
 #define SOUND_AK12_BURST_SD		SOUND_AK74_SD
 
 // AKS 74 U / Carbine *************************
 #define SOUND_AKSU				begin1[]={"\SOUND_PATH\acex_sound\aks74u_fire1",1.77828,1,1100};begin2[]={"\SOUND_PATH\acex_sound\aks74u_fire2",1.77828,1,1100};begin3[]={"\SOUND_PATH\acex_sound\aks74u_fire3",1.77828,1,1100};soundBegin[]={"begin1",0.33,"begin2",0.33,"begin3",0.33};
 #define SOUND_AKSU_SD			begin1[]={"\SOUND_PATH\acex_sound\aks74un_fire",1,1,80};begin2[]={"\SOUND_PATH\acex_sound\aks74un_fire",1,0.97,80};begin3[]={"\SOUND_PATH\acex_sound\aks74un_fire",1,1.016,80};soundBegin[]={"begin1",0.33,"begin2",0.33,"begin3"
+#define SOUND_AKSU_FA
+#define SOUND_AKSU_FA_SD
 
+#define SOUND_AK12U
+#define SOUND_AK12U_SD
+#define SOUND_AK12U_BURST
+#define SOUND_AK12U_BURST_SD
+#define SOUND_AK12U_FA
+#define SOUND_AK12U_FA_SD
 
 // AKM ****************************************
 #define SOUND_AKM				begin1[]={"\SOUND_PATH\acex_sound\akm_fire1",1.77828,1,1700};begin2[]={"\SOUND_PATH\acex_sound\akm_fire2",1.77828,1,1700};begin3[]={"\SOUND_PATH\acex_sound\akm_fire2",1.77828,1.027,1700};soundBegin[]={"begin1",0.33,"begin2",0.33,"begin3",0.33};
 #define SOUND_AKM_SD			SOUND_AK74_SD
+#define SOUND_AKM_FA			SOUND_AKM
+#define SOUND_AKM_FA_SD			SOUND_AK74_SD
 
 // RPK 762 ************************************
 #define SOUND_RPK				SOUND_AKM
 #define SOUND_RPK_SD			SOUND_AK74_SD
+#define SOUND_RPK_FA			SOUND_AKM
+#define SOUND_RPK_FA_SD			SOUND_AK74_SD
 
 // RPK74 **************************************
+#define SOUND_RPK12
+#define SOUND_RPK12_SD
+#define SOUND_RPK12_BURST
+#define SOUND_RPK12_BURST_SD
+#define SOUND_RPK12_FA
+#define SOUND_RPK12_FA_SD
+
 
 // PKM/PKP ************************************
 
