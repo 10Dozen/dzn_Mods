2aabcc8e85a2edf6dc7c4f40c9e1c9202781d83a
10Dozen <steel.mjr@gmail.com>
2015-04-07 00:18:01 +0300

    Update config.cpp

--- x_dzn_acex_hlcak/config.cpp
+++ x_dzn_acex_hlcak/config.cpp
@@ -37,20 +37,84 @@ class asdg_OpticRail1913_short;
 #define	STRNAME(NAME)	displayName = "NAME";displayNameShort = "NAME_short";descriptionShort = "NAME_desc";
 #define STRNAME2(NAME)	displayname = "NAME";descriptionShort = "NAME_desc";
 
-#define SOUND_AK74	begin1[]={"\x_dzn_acex_hlcak\acex_sound\ak74_fire1",1.77828,1,1100};begin2[]={"\x_dzn_acex_hlcak\acex_sound\ak74_fire2",1.77828,1,1100};begin3[]={"\x_dzn_acex_hlcak\acex_sound\ak74_fire3",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
-#define SOUND_AK74_SD	begin1[]={"\x_dzn_acex_hlcak\acex_sound\aks74un_fire",1,1,80};begin2[]={"\x_dzn_acex_hlcak\acex_sound\aks74un_fire",1,0.97,80};begin3[]={"\x_dzn_acex_hlcak\acex_sound\aks74un_fire",1,1.016,80};soundBegin[]={"begin1",0.33,"begin2",0.33,"begin3",0.33};	
-#define SOUND_AKSU	begin1[]={"\x_dzn_acex_hlcak\acex_sound\aks74u_fire1",1.77828,1,1100};begin2[]={"\x_dzn_acex_hlcak\acex_sound\aks74u_fire2",1.77828,1,1100};begin3[]={"\x_dzn_acex_hlcak\acex_sound\aks74u_fire3",1.77828,1,1100};soundBegin[]={"begin1",0.33,"begin2",0.33,"begin3",0.33};
-#define SOUND_AKSU_SD	begin1[]={"\x_dzn_acex_hlcak\acex_sound\aks74un_fire",1,1,80};begin2[]={"\x_dzn_acex_hlcak\acex_sound\aks74un_fire",1,0.97,80};begin3[]={"\x_dzn_acex_hlcak\acex_sound\aks74un_fire",1,1.016,80};soundBegin[]={"begin1",0.33,"begin2",0.33,"begin3"
-#define SOUND_AKM	begin1[]={"\x_dzn_acex_hlcak\acex_sound\akm_fire1",1.77828,1,1700};begin2[]={"\x_dzn_acex_hlcak\acex_sound\akm_fire2",1.77828,1,1700};begin3[]={"\x_dzn_acex_hlcak\acex_sound\akm_fire2",1.77828,1.027,1700};soundBegin[]={"begin1",0.33,"begin2",0.33,"begin3",0.33};
-#define SOUND_AKM_SD	SOUND_AK74_SD
-#define SOUND_RPK	SOUND_AKM
-#define SOUND_RPL_SD	SOUND_AK74_SD
-#define SOUND_AEK	SOUND_AK74
-#define SOUND_AEK_SD	SOUND_AK74_SD
-#define	SOUND_GP	sound[]={"\x_dzn_acex_hlcak\acex_sound\gp30_fire",1,1,100};
-#define	SOUND_GP_RELOAD	reloadMagazineSound[]={"\x_dzn_acex_hlcak\acex_sound\gp30_reload",1.0,1,10};
-#define SOUND_BURST	SOUND_AK74
-#define SOUND_BURST_SD	SOUND_AK74_SD
+
+
+#define SOUND_PATH  x_dzn_acex_hlcak
+// AK 74 **************************************
+#define SOUND_AK74				begin1[] = {"\SOUND_PATH\acex_sound\ak74_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\ak74_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\ak74_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AK74_SD			begin1[] = {"\SOUND_PATH\acex_sound\aks74un_fire.wss",1,1,80};begin2[] = {"\SOUND_PATH\acex_sound\aks74un_fire.wss",1,0.97,80};begin3[] = {"\SOUND_PATH\acex_sound\aks74un_fire.wss",1,1.016,80};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AK74_BURST		begin1[] = {"\SOUND_PATH\acex_sound\ak74_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\ak74_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\ak74_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AK74_BURST_SD		SOUND_AK74_SD
+#define SOUND_AK74_FA			begin1[] = {"\SOUND_PATH\acex_sound\ak74_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\ak74_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\ak74_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AK74_FA_SD		begin1[] = {"\SOUND_PATH\acex_sound\aks74un_fire.wss",1,1,80};begin2[] = {"\SOUND_PATH\acex_sound\aks74un_fire.wss",1,0.97,80};begin3[] = {"\SOUND_PATH\acex_sound\aks74un_fire.wss",1,1.016,80};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+
+#define SOUND_AEK				SOUND_AK74
+#define SOUND_AEK_SD			SOUND_AK74_SD
+#define SOUND_AEK_FA			SOUND_AK74_BURST
+#define SOUND_AEK_FA_SD			SOUND_AK74_BURST_SD
+#define SOUND_AEK_BURST			SOUND_AK74_FA
+#define SOUND_AEK_BURST_SD		SOUND_AK74_FA_SD
+
+#define SOUND_AK12				SOUND_AK74
+#define SOUND_AK12_SD			SOUND_AK74_SD
+#define SOUND_AK12_FA			SOUND_AK74_BURST
+#define SOUND_AK12_FA_SD		SOUND_AK74_BURST_SD
+#define SOUND_AK12_BURST		SOUND_AK74_FA
+#define SOUND_AK12_BURST_SD		SOUND_AK74_FA_SD
+
+// AKS 74 U / Carbine *************************
+#define SOUND_AKSU				begin1[] = {"\SOUND_PATH\acex_sound\aks74u_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\aks74u_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\aks74u_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AKSU_SD			begin1[] = {"\SOUND_PATH\acex_sound\aks74un_fire",1,1,80};begin2[]={"\SOUND_PATH\acex_sound\aks74un_fire",1,0.97,80};begin3[]={"\SOUND_PATH\acex_sound\aks74un_fire",1,1.016,80};soundBegin[]={"begin1",0.33,"begin2",0.33,"begin3"
+#define SOUND_AKSU_BURST		begin1[] = {"\SOUND_PATH\acex_sound\aks74u_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\aks74u_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\aks74u_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AKSU_BURST_SD		SOUND_AKSU_SD
+#define SOUND_AKSU_FA			begin1[] = {"\SOUND_PATH\acex_sound\aks74u_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\aks74u_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\aks74u_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AKSU_FA_SD		SOUND_AKSU_SD
+
+#define SOUND_AK12U				SOUND_AKSU
+#define SOUND_AK12U_SD			SOUND_AKSU_SD
+#define SOUND_AK12U_BURST		SOUND_AKSU_BURST
+#define SOUND_AK12U_BURST_SD	SOUND_AKSU_BURST_SD
+#define SOUND_AK12U_FA			SOUND_AKSU_FA
+#define SOUND_AK12U_FA_SD		SOUND_AKSU_FA_SD
+
+// AKM ****************************************
+#define SOUND_AKM				begin1[] = {"\SOUND_PATH\acex_sound\akm_fire1.wss",1.77828,1,1700};begin2[] = {"\SOUND_PATH\acex_sound\akm_fire2.wss",1.77828,1,1700};begin3[] = {"\SOUND_PATH\acex_sound\akm_fire2.wss",1.77828,1.027,1700};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AKM_SD			SOUND_AK74_SD
+#define SOUND_AKM_BURST			begin1[] = {"\SOUND_PATH\acex_sound\akm_fire1.wss",1.77828,1,1700};begin2[] = {"\SOUND_PATH\acex_sound\akm_fire2.wss",1.77828,1,1700};begin3[] = {"\SOUND_PATH\acex_sound\akm_fire2.wss",1.77828,1.027,1700};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AKM_BURST_SD		SOUND_AK74_BURST_SD
+#define SOUND_AKM_FA			begin1[] = {"\SOUND_PATH\acex_sound\akm_fire1.wss",1.77828,1,1700};begin2[] = {"\SOUND_PATH\acex_sound\akm_fire2.wss",1.77828,1,1700};begin3[] = {"\SOUND_PATH\acex_sound\akm_fire2.wss",1.77828,1.027,1700};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_AKM_FA_SD			SOUND_AK74_FA_SD
+
+// RPK 762 ************************************
+#define SOUND_RPK				SOUND_AKM
+#define SOUND_RPK_SD			SOUND_AK74_SD
+#define SOUND_RPK_FA			SOUND_AKM_FA
+#define SOUND_RPK_FA_SD			SOUND_AKM_FA_SD
+
+// RPK74 **************************************
+#define SOUND_RPK12				begin1[] = {"\SOUND_PATH\acex_sound\rpk74_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\rpk74_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\rpk74_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.34,"begin3",0.33};
+#define SOUND_RPK12_SD			SOUND_AK74_SD
+#define SOUND_RPK12_BURST		SOUND_RPK12
+#define SOUND_RPK12_BURST_SD	SOUND_AK74_BURST_SD
+#define SOUND_RPK12_FA			begin1[] = {"\SOUND_PATH\acex_sound\rpk74_fire1.wss",1.77828,1,1100};begin2[] = {"\SOUND_PATH\acex_sound\rpk74_fire2.wss",1.77828,1,1100};begin3[] = {"\SOUND_PATH\acex_sound\rpk74_fire3.wss",1.77828,1,1100};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_RPK12_FA_SD		SOUND_AK74_FA_SD
+
+// PKM/PKP ************************************
+#define SOUND_PKM				begin1[] = {"\SOUND_PATH\acex_sound\pkm_fire1.wss",1.77828,1,1700};begin2[] = {"\SOUND_PATH\acex_sound\pkm_fire2.wss",1.77828,1,1700};begin3[] = {"\SOUND_PATH\acex_sound\pkm_fire3.wss",1.77828,1,1700};soundBegin[] = {"begin1",0.33,"begin2",0.33,"begin3",0.33};
+#define SOUND_PKM_SD			SOUND_PKM
+
+#define SOUND_PKP				SOUND_PKM
+#define SOUND_PKP_SD			SOUND_PKM
+#define SOUND_PKP_FA			SOUND_PKM
+#define SOUND_PKP_FA_SD			SOUND_PKM
+
+// GP-25/30 ***********************************
+#define	SOUND_GP				sound[]={"\SOUND_PATH\acex_sound\gp30_fire.wss",1,1,100};
+#define	SOUND_GP_RELOAD			reloadMagazineSound[]={"\SOUND_PATH\acex_sound\gp30_reload.wss",0.0316228,1,20};
+
+
+
+
 
 // CONFIGS
 class CfgMagazines
