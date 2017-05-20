6fe2f346cdee060a0fec2b924ae218e8a2a0c209
10Dozen <steel.mjr@gmail.com>
2017-05-20 19:05:06 +0300

    Create config.cpp

---
+++ tS_ACE_Smoke/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+	class tS_ACE_Smoke
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"CBA_MAIN","ace_particles"};
+		author[] = {"10Dozen"};
+	};
+};
+
+class SmokeShellWhiteEffect
+{
+	class SmokeShell
+	{
+		type = "SmokeShellWhite";
+	};
+	class SmokeShell2
+	{
+		type = "SmokeShellWhite2";
+	};
+};
