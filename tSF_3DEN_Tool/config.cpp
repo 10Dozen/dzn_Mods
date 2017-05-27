64020ba2e7b97e16a9ac3c602bf6de99f7fe9c21
10Dozen <steel.mjr@gmail.com>
2017-05-27 13:32:00 +0300

    Update config.cpp

--- tSF_3DEN_Tool/config.cpp
+++ tSF_3DEN_Tool/config.cpp
@@ -5,37 +5,19 @@ class CfgPatches
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"CBA_MAIN"};
+		requiredAddons[] = {"3den"};
 		author[] = {"10Dozen"};
 	};
 };
 
-class Display3DEN
+class Cfg3DEN
 {
-	class Controls
+	class EventHandlers
 	{
-		class MenuStrip: ctrlMenuStrip
+		class dzn_3DENTool
 		{
-			class Items
-			{
-				#include "Enh_menuStrip_tools.hpp"
-				#include "Enh_menuStrip_help.hpp"
-			};
-		};
-	};
-	class ContextMenu: ctrlMenu
-	{
-		class Tools
-		{
-			items[] +=
-			{
-				"dzn_tSF_init"
-			};
-		};
-		class dzn_tSF_init
-		{
-			text = "Init tSF 3DEN Tool";
-			action = "[] execVM 'init3den.sqf';";
+			onTerrainNew = "if !(isNil { preProcessFileLineNumbers 'init3DEN.sqf' }) then { [] execVM 'init3den.sqf'; }";
+			onMissionPreviewEnd = "if !(isNil { preProcessFileLineNumbers 'init3DEN.sqf' }) then { [] execVM 'init3den.sqf'; }";
 		};
 	};
 };
