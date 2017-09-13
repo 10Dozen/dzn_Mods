1642fa6d2fd8899c540fdba8b0d36dd568c3af0a
10Dozen <steel.mjr@gmail.com>
2017-09-13 18:42:01 +0300

    Create config.cpp

---
+++ dzn_GearXtra/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+	class dzn_GearXtra
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"CBA_MAIN"};
+		magazines[] = {};
+		ammo[] = {};
+		
+		version = "V0.1";
+		author = "10Dozen";
+	};
+};
+
+class Extended_PostInit_EventHandlers
+{
+	class dzn_helmet_faceguard
+	{
+		clientInit = "call ('\dzn_GearXtra\init.sqf' call SLX_XEH_COMPILE)";
+	};
+};
