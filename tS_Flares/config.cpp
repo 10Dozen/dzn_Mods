ed2d2e06ba7e26d1e1f821d667a7c103946cff6e
10Dozen <steel.mjr@gmail.com>
2017-09-25 19:43:36 +0300

    Update config.cpp

--- tS_Flares/config.cpp
+++ tS_Flares/config.cpp
@@ -26,14 +26,18 @@ class Extended_PreInit_EventHandlers
 class Extended_Fired_Eventhandlers 
 {
 	// 82mm
+	class StaticMortar { SET_MORTAR_EH };
+	class Mortar_01_base_F { SET_MORTAR_EH };
+	/*
 	class CUP_B_M252_USMC { SET_MORTAR_EH };
 	class B_Mortar_01_F { SET_MORTAR_EH };
 	class RHS_M252_D { SET_MORTAR_EH };
 	class rhs_2b14_82mm_msv { SET_MORTAR_EH };
 	class CUP_O_2b14_82mm_RU { SET_MORTAR_EH };
 	
-	// 105mm
+	*/
 	/*
+	// 105mm
 	class rhs_ammo_m314_ilum { SET_HOWITZER_EH };
 	class CUP_Sh_105_ILLUM { SET_HOWITZER_EH };
 	
