96f4bd16957e418e07dfb0cb4850e50a56f4bf38
10Dozen <steel.mjr@gmail.com>
2018-04-03 09:52:06 +0300

    Config fixes
    - Mass of Magazine decreased to equal 3xGrenade
    - Added Stringtable and russian translation
    - Changed classname
    - Changed handAnim to CUP's Mk13

--- pook_gl/config.cpp
+++ pook_gl/config.cpp
@@ -13,12 +13,10 @@ class CfgPatches
 		magazines[] = {};
 		ammo[] = {};
 		
-		version = "v0.1";
+		version = "v0.2";
 		author = "pook & 10Dozen";
 	};
 };
-
-
 
 class Mode_SemiAuto;
 
@@ -36,19 +34,19 @@ class cfgWeapons
 	class CUP_glaunch_Mk13;
 	
 	
-	class dzn_weapon_GM94Proxy: CUP_glaunch_Mk13
+	class dzn_weapon_GM94: CUP_glaunch_Mk13
 	{
 		author = "pook & 10Dozen";
 		scope = 2;
-		handAnim[] = {"OFP2_ManSkeleton","\A3\Weapons_F\Rifles\Khaybar\data\anim\katiba.rtm"};
+		handAnim[] = {"OFP2_ManSkeleton","\CUP\Weapons\CUP_Weapons_GrenadeLaunchers\data\anim\Mk13.rtm"};
 		picture = "\pook_gl\data\icon\pook_GM94.paa";
 		model = "\pook_gl\pook_GM94.p3d";
-		displayName = "GM-94 (Proxy)";
+		displayName = "$STR_dzn_pook_GM94";
 		class Library
 		{
-			libTextDesc = "GM-94 (Proxy) 43mm pump-action grenade launcher";
+			libTextDesc = "$STR_dzn_pook_GM94_desc";
 		};
-		descriptionShort = "GM-94 (Proxy) 43mm pump-action grenade launcher";
+		descriptionShort = "$STR_dzn_pook_GM94_desc";
 		cameraDir = "OP_look";
 		discreteDistance[] = {150};
 		discreteDistanceCameraPoint[] = {"OP_eye_150"};
@@ -62,14 +60,14 @@ class cfgWeapons
 			class BaseSoundModeType;
 			class StandardSound: BaseSoundModeType
 			{
-				begin1[] = {"\pook_gl\data\sound\gm94_fire.ogg",2.1,1,1100};			
+				begin1[] = {"\pook_gl\data\sound\gm94_fire.ogg",2.3,1,1100};			
 				soundBegin[] = {"begin1",1};
 			};
 			reloadTime = 0.75;
-			magazineReloadTime = 5;
+			magazineReloadTime = 7;
 		};
 		magazines[] = {"dzn_3Rnd_HE_GM94"};
-		magazineReloadTime = 5;
+		magazineReloadTime = 7;
 		//reloadMagazineSound[] = {"\pook_gl\data\sound\gm94_reload.ogg",1.77828,1,1100};
 	};
 };
@@ -88,7 +86,6 @@ class CfgAmmo
 	};
 };
 
-// Magazines
 class CfgMagazines
 {
 	class CA_Magazine;
@@ -98,33 +95,10 @@ class CfgMagazines
 	class dzn_3Rnd_HE_GM94: 1Rnd_HE_Grenade_shell
 	{
 		ammo = "dzn_43mm_HE";
-		displayName = "43mm HE grenade pack";
-		descriptionShort = "3x 43mm VGM93.100 grenades for GM-94";
-		mass = 22;
+		displayName = "$STR_dzn_pook_GM94_3Rnd_Mag";
+		descriptionShort = "$STR_dzn_pook_GM94_3Rnd_Mag_desc";
+		mass = 13;
 		count = 3;
 		picture = "\pook_gl\data\icon\vgm93_100.paa";
 	};
 };
-
-/*
-	
-	
-	Textures
-	
-	11:09:13 Warning Message: Cannot load texture pook_gl\data\gm94.paa.
-11:09:13 Warning Message: Cannot load texture pook_gl\data\rg6_co.paa.
-11:09:13 Warning Message: Cannot load texture pook_gl\data\ti_nohq.paa.
-11:09:13 Warning Message: Cannot load texture pook_gl\data\ti_as.paa.
-11:09:13 Warning Message: Cannot load texture pook_gl\data\ti_smdi.paa.
-11:09:13 Warning Message: Cannot load texture pook_gl\data\rg6_nohq.paa.
-11:09:13 Warning Message: Cannot load texture pook_gl\data\rg6_smdi.paa.
-*/
-
-	
-	
-	/*
-	
-	HAND ANIM from
-		"arifle_Katiba_F"
-		
-		
