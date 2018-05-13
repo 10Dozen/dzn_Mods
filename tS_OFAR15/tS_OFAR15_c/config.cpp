d01e56a9be205b778cc072bc08d61b258f6bc8ae
10Dozen <steel.mjr@gmail.com>
2018-05-14 00:56:44 +0300

    Update config.cpp

--- tS_OFAR15/tS_OFAR15_c/config.cpp
+++ tS_OFAR15/tS_OFAR15_c/config.cpp
@@ -27,7 +27,7 @@ class CfgPatches
 		};
 		
 		requiredVersion = 0.1;
-		requiredAddons[] = {"A3_Data_F","A3_Weapons_F","CBA_A3","cba_jr","hlcweapons_core"};
+		requiredAddons[] = {"A3_Data_F","A3_Weapons_F","cba_jr","hlcweapons_core"};
 		
 		version = "v0.1";
 		author = "RobertHammer & toadie & 10Dozen";
